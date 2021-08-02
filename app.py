99a91c1e40e76f908d6a54e8fb763fa309ccaa84
CharlesAndrea <charles.andrea@correo.unimet.edu.ve>
2021-08-02 13:23:47 -0400

    mods

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 api = Api(app)
+
+app.config.from_object('config')
+
+
+db = SQLAlchemy()
+db.init_app(app)
+migrate = Migrate(app, db)
+
 
 class HelloWorld(Resource):
     def get(self):
