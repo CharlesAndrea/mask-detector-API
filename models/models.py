e7b44f8295770e33e98a6145609bbc50f287017b
CharlesAndrea <charles.andrea@correo.unimet.edu.ve>
2021-08-04 12:14:55 -0400

    models

--- models/models.py
+++ models/models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import backref
 
 db = SQLAlchemy
 
@@ -14,7 +15,10 @@ class Empleado(db.Model):
     fecha_nacimiento = db.Column(db.String)
     sexo = db.Column(db.String)
     estado = db.Column(db.Boolean)
-    historiales = db.relationship('Historial', backref='empleado', lazy=True)
+    dept_id = db.Column(db.Integer, db.ForeignKey('departamento.id'), nullable=False)
+    rol_id = db.Column(db.Integer, db.ForeignKey('rol.id'), nullable=False)
+    ci_s = Column(Integer, ForeignKey('empleado.id'))
+    parent = relationship('Empleado', remote_side=[ci])
     @property
     def serialize(self):
         return {
@@ -26,13 +30,15 @@ class Empleado(db.Model):
             'fecha_nacimiento': self.fecha_nacimiento,
             'sexo': self.sexo,
             'estado': self.estado,
-            'historiales': self.historiales
+            'dept_id': self.dept_id,
+            'rol_id': self.rol_id
         }
 
 class Departamento(db.Model):
     __tablename__ = 'departamento'
     id = db.Column(db.Integer, primary_key=True)
     nombre_dept = db.Column(db.String)
+    empleados = db.relationship('Empleado', backref='departamento', lazy=True)
     @property
     def serialize(self):
         return {
@@ -44,31 +50,30 @@ class Rol(db.Model):
     __tablename__ = 'rol'
     id = db.Column(db.Integer, primary_key=True)
     nombre_rol = db.Column(db.String)
+    empleados = db.relationship('Empleado', backref='rol', lazy=True)
     @property
     def serialize(self):
         return {
             'id': self.id,
-            'nombre_rol': self.nombre_rol
+            'nombre_rol': self.nombre_rol,
+            'empleados': self.empleados
         }
 
 class Historial(db.Model):
     __tablename__= 'historial'
     id = db.Column(db.Integer)
-    ci_empleado = db.Column(db.Integer, db.ForeignKey('empleado.ci'),
-        nullable=False)
     modo_uso = db.Column(db.String)
     fecha = db.Column(db.DateTime)
     @property
     def serialize(self):
         return {
             'id': self.id,
-            'ci_empleado': self.ci_empleado,
             'modo_uso': self.modo_uso,
             'fecha': self.fecha
         }
 
 
 historial_generado = db.Table('historial_generado',
-    db.Column('ci_empleado', db.Integer, db.ForeignKey('empleado.ci'), primary_key=True),
+    db.Column('ci_e', db.Integer, db.ForeignKey('empleado.ci'), primary_key=True),
     db.Column('id_hist', db.Integer, db.ForeignKey('historial.id'), primary_key=True)
 )
